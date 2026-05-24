66a2eb50075f00ff2fafeb4f9ad14dc39cebaff2
User <User@PC302-17>
2026-05-24 16:02:43 +0300

    ףלהנו

--- Ilai Sheffer's WebSite/שליפת מקום אימוץ.aspx.cs
+++ Ilai Sheffer's WebSite/שליפת מקום אימוץ.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,33 +10,38 @@ using System.Xml.Linq;
 
 public partial class שליפת_מקום_אימוץ : System.Web.UI.Page
 {
-    public string st = "";
+    public string stadp = "";
     protected void Page_Load(object sender, EventArgs e)
     {
-        string sql = "Select * from Adoptionplaces";
-
+        String sql = "Select [Adoptionplacename],[animalsforadoption],[animalsforadoption],[placephonenumber],[Adoptioncenteraddress] from Adoptionplaces";
         DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
         if (dt.Rows.Count == 0)
         {
-            st = "אין נתונים";
+            stadp = "אין נתונים";
         }
         else
         {
 
-            st = "<table border='1' style='border-collapse:collapse; width:100%; font-size:24px; text-align:center'>";
-
+            stadp = "<table border='1' style='border-collapse:collapse; width:100%; font-size:24px; text-align:center'>";
+            stadp += "<tr>";
+            stadp += "<td>שם מקום אימוץ</td>";
+            stadp += "<td>חיות לאימוץ</td>";
+            stadp += "<td>טלפון מקום אימוץ </td>";
+            stadp += "<td>כתובת מקום אימוץ</td>";
+            stadp += "<td>מידע נוסף </td>";
+            stadp += "</tr>";
             for (int i = 0; i < dt.Rows.Count; i++)
             {
-                st += "<tr>";
+                stadp += "<tr>";
                 for (int j = 0; j < dt.Columns.Count; j++)
                 {
-                    st += "<td style='border: 3px solid black'>";
-                    st += dt.Rows[i][j];
-                    st += "</td>";
+                    stadp += "<td style='border: 3px solid black'>";
+                    stadp += dt.Rows[i][j];
+                    stadp += "</td>";
                 }
-                st += "</tr>";
+                stadp += "</tr>";
             }
-            st += "</table>";
+            stadp += "</table>";
         }
     }
 }
